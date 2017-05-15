880af946cb116d83aced9ec9ad20a6d4972d15df
Jason Sites <email@jasonsites.com>
2017-05-14 22:26:54 -0600

    fix: removes deprecated dirs

--- src/example/index.js
+++
@@ -1,8 +0,0 @@
-import Bluebird from 'bluebird'
-
-export default function handler() {
-  return Bluebird.try(() => {
-    const data = { data: 'Example response message' }
-    return data
-  })
-}
--- src/http/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-import koaBunyanLogger from 'koa-bunyan-logger'
-
-import { name, version } from '../../../package.json'
-
-export default function logger(config) {
-  const level = config.get(['log', 'level'])
-  return koaBunyanLogger({ name: `${name}/${version}`, level })
-}
