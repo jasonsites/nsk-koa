1908d1020edf55b7ca9ea7fa5df06ff0f6e5922a
Jason Sites <email@jasonsites.com>
2017-05-20 19:23:25 -0600

    fix: adds basic feature test structure

---
+++ test/unit/feature.test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+import handler from '../../src/feature'
+
+describe('[feature]', function () {
+  it('should return correct data', function () {
+    return handler()
+    .then((data) => {
+      console.log(JSON.stringify(data))
+      expect(data).to.deep.equal({ data: 'feature data example' })
+    })
+  })
+})
