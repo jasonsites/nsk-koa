b4ad24d37b1b39b5866734190041ca95d0a77fe5
Jason Sites <email@jasonsites.com>
2018-07-31 14:20:08 -0600

    fix: nests modules in directories to help differentiate them from root source files

---
+++ src/domain/index.js
@@ -0,0 +1,25 @@
+
+module.exports = function domain(repo) {
+  async function create({ data }) {
+    return repo.create({ data })
+  }
+
+  async function destroy({ id }) {
+    return repo.destroy({ id })
+  }
+
+  async function detail({ id }) {
+    return repo.get({ id })
+  }
+
+  async function update({ data }) {
+    return repo.update({ data })
+  }
+
+  return { create, destroy, detail, update }
+}
+
+module.exports.inject = {
+  name: 'domain',
+  require: ['repo'],
+}
---
+++ src/logger/index.js
@@ -0,0 +1,13 @@
+const { createLogger } = require('bunyan')
+const config = require('config')
+
+const { name, version } = require('../../package.json')
+
+module.exports = function logger() {
+  const level = config.get('logger.app.level')
+  return createLogger({ level, name, version })
+}
+
+module.exports.inject = {
+  name: 'logger',
+}
