d4bd201d4ebca7f3e8f52a409ec881eead9020ce
Jason Sites <email@jasonsites.com>
2019-05-18 15:48:28 -0600

    fix(validation): restructures validation module to allow for future extensibility

--- src/validation/index.js
+++ src/validation/index.js
@@ -4,53 +4,33 @@
  */
 
 const { badRequest } = require('boom')
+const config = require('config')
 const joi = require('@hapi/joi')
 
-module.exports = function createValidator() {
+const { entitySchema } = require('./schemas')
+
+module.exports = function createValidator({ logger }) {
+  const { active, label, level } = config.get('logger.validation')
+  const log = logger.child({ level, service: label })
+
   function validate({ body, method }) {
     try {
-      const schema = generateSchema(method)
+      const schema = entitySchema(method)
       return joi.attempt(body, schema)
-    } catch (err) {
-      const msg = Array.isArray(err.details)
-        ? err.details.map(detail => detail.message).join(', ')
-        : err.message
-      throw badRequest(`Invalid request body: ${msg}`)
+    } catch (error) {
+      const message = Array.isArray(error.details)
+        ? error.details.map(detail => detail.message).join(', ')
+        : error.message
+      if (active) log.error(`'${message}'`)
+      throw badRequest(`${message}`)
     }
   }
   return { validate }
 }
 
-/**
- * Create joi validation schema for POST/PATCH request bodies
- * @param  {String} method - http request method
- * @return {Object}
- */
-function generateSchema(method) {
-  const dataTypes = [joi.boolean(), joi.number()]
-  if (method === 'PATCH') dataTypes.push(joi.string().allow(null))
-  else dataTypes.push(joi.string())
-
-  return joi.object({
-    jsonapi: joi.object({
-      version: joi.string(),
-    }).required(),
-    meta: joi.object({
-      // no restrictions on field names
-    }).unknown(true),
-    data: joi.object({
-      type: joi.string().valid('entity').required(),
-      id: joi.string().required(),
-      attributes: joi.object({
-        domain: joi.object({
-          // no restrictions on field names
-        }).required()
-          .pattern(/.*/, joi.alternatives(dataTypes)),
-      }).required(),
-    }).required(),
-  })
-}
-
 module.exports.inject = {
   name: 'validation',
+  require: {
+    logger: 'logger',
+  },
 }
---
+++ src/validation/schemas/index.js
@@ -0,0 +1,40 @@
+/**
+ * @file validation/schemas/index.js
+ * @overview schemas
+ */
+
+const joi = require('@hapi/joi')
+
+/**
+ * Create joi validation schema for POST/PATCH request bodies
+ * @param  {String} method - http request method
+ * @return {Object}
+ */
+function entitySchema(method) {
+  const dataTypes = [joi.boolean(), joi.number()]
+  if (method === 'PATCH') dataTypes.push(joi.string().allow(null))
+  else dataTypes.push(joi.string())
+
+  return joi.object({
+    jsonapi: joi.object({
+      version: joi.string(),
+    }).required(),
+    meta: joi.object({
+      // no restrictions on field names
+    }).unknown(true),
+    data: joi.object({
+      type: joi.string().valid('entity').required(),
+      id: joi.string().required(),
+      attributes: joi.object({
+        domain: joi.object({
+          // no restrictions on field names
+        }).required()
+          .pattern(/.*/, joi.alternatives(dataTypes)),
+      }).required(),
+    }).required(),
+  })
+}
+
+module.exports = {
+  entitySchema,
+}
