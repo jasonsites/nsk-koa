475f71092eedd9e455b9a555b2c1f42775adf761
Jason Sites <email@jasonsites.com>
2019-11-08 23:09:36 -0700

    test: refactors correlation context wrapper functions to improve testability

--- src/http/validation/index.js
+++ src/http/validation/index.js
@@ -10,71 +10,73 @@ module.exports = function validation({ core, logger, schemas }) {
 
   const { enabled, label, level } = config.get('logger.validation')
 
-  return (correlation) => {
-    const { req_id } = correlation
-    const log = logger.child({ module: label, req_id, level })
-    log.enabled = enabled
+  return {
+    context: (correlation) => {
+      const { req_id } = correlation
+      const log = logger.child({ module: label, req_id, level })
+      log.enabled = enabled
 
-    function composeValidationError({ details, messages }) {
-      const error = new ValidationError(messages.join(', '))
-      error.details = details
-      return error
-    }
-
-    function formatBasicValidationErrors({ error }) {
-      if (!error || !error.isJoi || !Array.isArray(error.details)) {
-        throw new Error('unknown validation error')
+      function composeValidationError({ details, messages }) {
+        const error = new ValidationError(messages.join(', '))
+        error.details = details
+        return error
       }
-      return error.details.reduce((memo, detail) => {
-        const { message, path } = detail
-        memo.messages.push(message)
-        const data = {
-          status: 400,
-          source: { pointer: path.map((p) => `/${p}`).join('') },
-          title: error.name,
-          detail: message,
+
+      function formatBasicValidationErrors({ error }) {
+        if (!error || !error.isJoi || !Array.isArray(error.details)) {
+          throw new Error('unknown validation error')
         }
-        memo.details.push(data)
-        return memo
-      }, { details: [], messages: [] })
-    }
-
-    function throwOnInvalid({ errors }) {
-      if (errors.details.length) {
-        const err = composeValidationError(errors)
-        if (log.enabled === 'true') log.error(`${err.message}`)
-        throw err
+        return error.details.reduce((memo, detail) => {
+          const { message, path } = detail
+          memo.messages.push(message)
+          const data = {
+            status: 400,
+            source: { pointer: path.map((p) => `/${p}`).join('') },
+            title: error.name,
+            detail: message,
+          }
+          memo.details.push(data)
+          return memo
+        }, { details: [], messages: [] })
       }
-    }
 
-    function validateBody({ body, method, type }) {
-      const schema = schemas.bodySchema({ method, type })
-      const options = { abortEarly: false }
-      const { error } = schema.validate(body, options)
+      function throwOnInvalid({ errors }) {
+        if (errors.details.length) {
+          const err = composeValidationError(errors)
+          if (log.enabled === 'true') log.error(`${err.message}`)
+          throw err
+        }
+      }
 
-      const errors = error
-        ? formatBasicValidationErrors({ error })
-        : { details: [], messages: [] }
-      throwOnInvalid({ log, errors })
-    }
+      function validateBody({ body, method, type }) {
+        const schema = schemas.bodySchema({ method, type })
+        const options = { abortEarly: false }
+        const { error } = schema.validate(body, options)
 
-    function validateQuery({ query, type }) {
-      const schema = schemas.querySchema({ type })
-      const options = { abortEarly: false }
-      const { error } = schema.validate(query, options)
+        const errors = error
+          ? formatBasicValidationErrors({ error })
+          : { details: [], messages: [] }
+        throwOnInvalid({ log, errors })
+      }
 
-      const errors = error
-        ? formatBasicValidationErrors({ error })
-        : { details: [], messages: [] }
-      throwOnInvalid({ errors })
-    }
+      function validateQuery({ query, type }) {
+        const schema = schemas.querySchema({ type })
+        const options = { abortEarly: false }
+        const { error } = schema.validate(query, options)
 
-    return {
-      composeValidationError,
-      formatBasicValidationErrors,
-      validateBody,
-      validateQuery,
-    }
+        const errors = error
+          ? formatBasicValidationErrors({ error })
+          : { details: [], messages: [] }
+        throwOnInvalid({ errors })
+      }
+
+      return {
+        composeValidationError,
+        formatBasicValidationErrors,
+        validateBody,
+        validateQuery,
+      }
+    },
   }
 }
 
